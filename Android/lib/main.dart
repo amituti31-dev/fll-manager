e26f0a935ca6ec1644406b6e4d368c80d7b61d7f
amituti31-dev <amituti31@gmail.com>
2026-06-25 17:13:42 +0300

    Release v1.0.6

--- Android/lib/main.dart
+++ Android/lib/main.dart
@@ -65,7 +65,7 @@ class _RootRouter extends StatefulWidget {
 }
 
 class _RootRouterState extends State<_RootRouter> {
-  static const _currentVersion = '1.0.0';
+  static const _currentVersion = '1.0.6';
   bool _updateChecked = false;
 
   bool _isNewer(String remote) {
